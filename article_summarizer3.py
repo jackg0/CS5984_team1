cc5cf25ea798f52877c542b9a1d61f15ea067b34
Jack Geissinger <jackg7@vt.edu>
2018-10-19 11:35:16 -0400

    cleaned up code, started using a slots dict

--- article_summarizer3.py
+++ article_summarizer3.py
@@ -1,9 +1,5 @@
-import spacy
-import requests, re
-import time
-import json
+import spacy, re, time, json
 from collections import *
-from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,28 +13,13 @@ Load in small dataset and grab sentences
 with open('archive_spark_hurricane_output.json') as file:
     data = json.load(file)
 
-noise = ("STAAR", "Getty", "People", "Photo:", "More:", "Subscribe", "SUBSCRIBE", "SHARE", "Tags", "RELATED:", "MORE:", "100-year floodplain", "Image")
-sentences_list = ['' if any([term in articles['Sentences'] for term in noise]) else articles['Sentences'] for articles in data]
+noise = ("STAAR", "Getty", "People", "Photo:", "More:", "Subscribe", "SUBSCRIBE",
+    "SHARE", "Tags", "RELATED:", "MORE:", "100-year floodplain", "Image")
+sentences_list = ['' if any([term in articles['Sentences'] for term in noise])
+    else articles['Sentences'] for articles in data]
 
 sentences_list = sentences_list[:len(sentences_list)]
 sentences = (' ').join(sentences_list)
-
-'''
-Import URLs for extra text to add into the mix.
-'''
-# source: https://towardsdatascience.com/named-entity-recognition-with-nltk-and-spacy-8c4a7d88e7da
-def url_to_string(url):
-    res = requests.get(url)
-    html = res.text
-    soup = BeautifulSoup(html, 'html5lib')
-    for script in soup(['script', 'style', 'aside']):
-        script.extract()
-    return ' '.join(re.split(r'[\n\t]+', soup.get_text()))
-
-cnn_sentences = url_to_string('https://www.cnn.com/specials/us/hurricane-harvey')
-
-sentences = sentences + ' ' + cnn_sentences
-
 
 '''
 Throw sentences into spacy and make a hashmap for all of the named entities
@@ -50,56 +31,38 @@ entsHash = {}
 
 for ent in ents:
     if ent.label_ not in entsHash:
-        entsHash[ent.label_] = {}
+        entsHash[ent.label_] = OrderedDict()
         entsHash[ent.label_][ent.text] = 1
     else:
         if ent.text not in entsHash[ent.label_]:
             entsHash[ent.label_][ent.text] = 1
         else:
             entsHash[ent.label_][ent.text] += 1
+'''
+Define slots dict to store information
+'''
+slots = {}
 
 '''
-Choose most frequent event, which will correspond to the hurricane we are studying
+Entities expected to be chosen based on most frequent
 '''
-events = sorted(entsHash['EVENT'].items(), key = lambda item : item[1], reverse=True)
-main_event = events[0][0]
+ent_freq = ['EVENT', 'LOC', 'GPE', 'ORG', 'MONEY']
 
-'''
-Choose most frequent location for where it traveled, and most common ocean for where it originated
-'''
-locations = sorted(entsHash['LOC'].items(), key = lambda item : item[1], reverse=True)
-main_loc = locations[0][0]
-
-oceans = ['Atlantic', 'Pacific', 'Arctic', 'Indian', 'Southern']
-for loc in locations:
-    if any([ocean in loc[0][0] for ocean in oceans]):
-        ocean = loc[0][0]
-        break
-
-'''
-Choose most frequently cited city or state related to the event
-'''
-cities_states = sorted(entsHash['GPE'].items(), key = lambda item : item[1], reverse=True)
-main_city_state = cities_states[0][0]
-
-'''
-Choose organization most involved with the event
-'''
-orgs = sorted(entsHash['ORG'].items(), key = lambda item : item[1], reverse=True)
-main_org = orgs[0][0]
-
-'''
-Choose most frequently cited cost associated with the event
-'''
-damages = sorted(entsHash['MONEY'].items(), key = lambda item : item[1], reverse=True)
-main_damage = damages[0][0]
+for ent_type in ent_freq:
+    item = max(entsHash[ent_type].items(),
+        key = lambda item : item[1])
+    slots[ent_type] = item[0]
 
 '''
 Choose most frequently cited month associated with the event
 '''
-month_abbrev = {'January' : ('January', 'Jan.', 'Jan'), 'February' : ('February', 'Feb.', 'Feb'), 'March' : ('Mar', 'March'), 'April' : ('Apr', 'April'),
-    'May' : ('May','May'), 'June' : ('Jun', 'Jun.', 'June'), 'July' : ('Jul', 'July'), 'August' : ('August', 'Aug.', 'Aug'), 'September' : ('September', 'Sept', 'Sept.'),
-    'October' : ('October', 'Oct', 'Oct.'), 'November' : ('November', 'Nov', 'Nov.'), 'December' : ('December', 'Dec', 'Dec.')}
+month_abbrev = {'January' : ('January', 'Jan.', 'Jan'),
+    'February' : ('February', 'Feb.', 'Feb'), 'March' : ('Mar', 'March'),
+    'April' : ('Apr', 'April'), 'May' : ('May','May'),     'June' : ('Jun', 'Jun.', 'June'),
+    'July' : ('Jul', 'July'), 'August' : ('August', 'Aug.', 'Aug'),
+    'September' : ('September', 'Sept', 'Sept.'), 'October' : ('October', 'Oct', 'Oct.'),
+    'November' : ('November', 'Nov', 'Nov.'), 'December' : ('December', 'Dec', 'Dec.')}
+
 months = defaultdict(int)
 
 for month in month_abbrev.keys():
@@ -108,7 +71,7 @@ for month in month_abbrev.keys():
             months[month] += item[1]
 
 months = sorted(months.items(), key = lambda item : item[1], reverse=True)
-main_month = months[0][0]
+slots['MONTH'] = months[0][0]
 
 '''
 Choose most frequently cited year associated with the event
@@ -122,46 +85,10 @@ for item in entsHash['DATE'].items():
         years[year] += 1
         if years[year] > max_count:
             max_count = years[year]
-            main_year = year
+            slots['YEAR'] = year
 
 '''
-Evaluate inches of rainfall and then place rainfall in bins with
-matplotlib and report the bin edges with the most frequency.
-'''
-inches = []
-for quantity in entsHash['QUANTITY'].items():
-    if 'inches' in quantity[0]:
-        numbers = re.findall(r'\d+',quantity[0])
-        if len(numbers) == 1:
-            inches += numbers*quantity[1]
-
-inches = [int(inch) for inch in inches]
-inches.sort()
-
-(n, bins, patches) = plt.hist(inches, bins='auto')
-idx = np.argmax(n)
-rainfall_range = [int(bins[idx]), int(bins[idx+1])]
-
-'''
-Evaluate the windspeed of the Hurricane and determine most frequent range
-of windspeeds if binned by matplotlib.
-'''
-windspeeds = []
-for quantity in entsHash['QUANTITY'].items():
-    if 'mph' in quantity[0]:
-        numbers = re.findall(r'\d+', quantity[0])
-        if len(numbers) == 1:
-            windspeeds += numbers*quantity[1]
-
-windspeeds = [int(windspeed) for windspeed in windspeeds]
-windspeeds.sort()
-
-(n, bins, patches) = plt.hist(windspeeds, bins='auto')
-idx = np.argmax(n)
-windspeed_range = [int(bins[idx]), int(bins[idx+1])]
-
-'''
-Find most frequent mention of Category in QUANTITY
+Choose most frequently cited Category associated with the event
 '''
 category = defaultdict(int)
 max_count = 0
@@ -170,14 +97,39 @@ for quantity in entsHash['QUANTITY'].items():
         category[quantity[0]] += quantity[1]
         if category[quantity[0]] > max_count:
             max_count = category[quantity[0]]
-            main_category = quantity[0]
+            slots['CATEGORY'] = quantity[0]
 
-print("{} was a {} hurricane that made landfall on {}, {} in {}.".format(main_event, main_category, main_month, main_year, main_city_state))
-print("The hurricane traveled through {} with windspeeds that ranged from a low of {} mph to a peak of {} mph.".format(main_loc, windspeed_range[0], windspeed_range[1]))
-print("In addition, the rainfall from {} varied in areas, but there seemed to be around {} inches to {} inches in many areas.".format(main_event, rainfall_range[0], rainfall_range[1]))
-print("{} was primarily involved with dealing with the affected area, and {} in damage was caused.".format(main_org, main_damage))
+'''
+range_quantities(type) will find inches, mph, or other quantities,
+make an array of numbers associated with those quantities,
+bin them using matplotlib, and find a range for the most frequent
+quantities.
+'''
+def range_quantities(type):
+    numbers = []
+    for quantity in entsHash['QUANTITY'].items():
+        if type in quantity[0]:
+            number = re.findall(r'\d+', quantity[0])
+            if len(number) == 1:
+                numbers += number*quantity[1]
+    numbers = [int(number) for number in numbers]
+    numbers.sort()
 
+    (n, bins, patches) = plt.hist(numbers, bins='auto')
+    idx = np.argmax(n)
+    ranges = [int(bins[idx]), int(bins[idx+1])]
+    return ranges
 
-print(sorted(entsHash['LOC'].items(), key = lambda item : item[1], reverse=True))
+slots['RAINFALL'] = range_quantities('inches')
+slots['WINDSPEED'] = range_quantities('mph')
 
+print("{} was a {} hurricane that made landfall in {}, {} in {}.".format(slots['EVENT'],
+    slots['CATEGORY'], slots['MONTH'], slots['YEAR'], slots['GPE']))
+print("The hurricane traveled through {} with windspeeds that ranged from a low of {} mph to a peak of {} mph.".format(slots['LOC'],
+    slots['WINDSPEED'][0], slots['WINDSPEED'][1]))
+print("In addition, the rainfall from {} varied in areas, but there seemed to be around {} inches to {} inches in many areas.".format(slots['EVENT'],
+    slots['RAINFALL'][0], slots['RAINFALL'][1]))
+print("{} was primarily involved with dealing with the affected area, and {} in damage was caused.".format(slots['ORG'], slots['MONEY']))
+
+print()
 print("Time elapsed: {}".format(time.time() - t0))
